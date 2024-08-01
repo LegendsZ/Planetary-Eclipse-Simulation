00844af1d082e834b837d3a6d5e129e60e6a364a
LegendsZ <akkshai@yahoo.com>
2024-08-02 01:26:07 +0530

    [US] initial env setup, GLAD not working

---
+++ src/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../include/_Window.h"
+
+int main(int argc, char* argv[]){
+	std::cout << "Starting PlanetarySimulation...";
+	std::cout << "OK!\n";
+
+
+
+	std::cout << "Goodbye!\n";
+	return 0;
+}
